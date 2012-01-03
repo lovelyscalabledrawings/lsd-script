e8ec047b3f1daed348c61742c370b9b025027902
Yaroslaff Fedin <invizko@gmail.com>
2012-01-03 09:44:02 -0300

    Make grouping happen after LSD.Object setter. The setter now may halt the grouping. Move LSD.Object.Array to LSD.Object.Group.Array

--- Source/Object/Object.Group.js
+++ Source/Object/Object.Group.js
@@ -11,10 +11,14 @@ authors: Yaroslaff Fedin
 
 requires:
   - LSD.Object
+  - LSD.Struct
+  - LSD.Array
 
 provides:
   - LSD.Object.Group
-
+  - LSD.Object.Group.Array
+  - LSD.Struct.Group
+  - LSD.Struct.Group.Array
 ...
 */
 
@@ -26,61 +30,89 @@ LSD.Object.Group = function(object) {
 LSD.Object.Group.prototype = {
   _constructor: LSD.Object.Stack,
   
-  set: function(key, value, memo, prepend) {
-    if (typeof key === 'string') {
+  set: function(key, value, memo, prepend, hash) {
+    if (typeof key === 'string' && hash !== true) {
       var index = key.indexOf('.');
     } else {
-      var hash = this._hash(key);
+      if (hash == null || hash === true) hash = this._hash(key, value);
       if (typeof hash == 'string') {
         key = hash;
+        hash = null;
         var index = key.indexOf('.');
-      } else {
-        var group = hash;
       }
     }
-    if (!group && index == -1 && key.charAt(0) != '_' && !(this._properties && this._properties[key])) {
-      var group = this[key];
-      if (!group) group = this[key] = new (this.___constructor || Array);
+    if (index === -1 && hash == null && key.charAt(0) != '_' && !(this._properties && this._properties[key])) {
+      hash = this[key];
+      if (hash == null) hash = this[key] = new (this.___constructor || Array);
     }
-    if (group) (prepend || value == null) ? group.unshift(value) : group.push(value);
-    return this._set(key, value, memo, index, group);
+    if (typeof (this._set(key, value, memo, index, hash)) != 'undefined') {
+      if (hash != null) (prepend || value == null) ? hash.unshift(value) : hash.push(value);
+      return true;
+    }
   },
   
-  unset: function(key, value, memo, prepend, bypass) {
-    if (typeof key === 'string') {
+  unset: function(key, value, memo, prepend, hash) {
+    if (typeof key === 'string' && hash !== true) {
       var index = key.indexOf('.');
     } else {
-      var hash = this._hash(key);
+      if (hash == null || hash === true) hash = this._hash(key, value);
       if (typeof hash == 'string') {
         key = hash;
+        hash = null;
         var index = key.indexOf('.');
       } else {  
-        if (hash == null) return false;
-        var group = hash;
+        if (hash == null) return;
       }
     }
-    if (!group && index == -1 && key.charAt(0) != '_' && !(this._properties && this._properties[key]))
-      var group = this[key];
-    if (!group) return;
-    var length = group.length;
-    if (prepend) {
-      for (var i = 0, j = length; i < j; i++)
-        if (group[i] === value) {
-          group.splice(i, 1);
-          break;
-        }
-      if (j == i) return false
-    } else {
-      for (var j = length; --j > -1;)
-        if (group[j] === value) {
-          group.splice(j, 1);
-          break;
-        }
-      if (j == -1) return false;
+    if (index === -1 && hash == null && key.charAt(0) != '_' && !(this._properties && this._properties[key]))
+      hash = this[key];
+    if (hash == null) return;
+    var length = hash.length;
+    if (typeof (this._unset(key, value, memo, index, hash)) != 'undefined') {
+      if (prepend) {
+        for (var i = 0, j = length; i < j; i++)
+          if (hash[i] === value) {
+            hash.splice(i, 1);
+            break;
+          }
+        if (j == i) return
+      } else {
+        for (var j = length; --j > -1;)
+          if (hash[j] === value) {
+            hash.splice(j, 1);
+            break;
+          }
+        if (j == -1) return;
+      }
+      return true;
     }
-    this._unset(key, value, memo, index, group);
-    return true;
   }
 };
 
-LSD.Object.Group.prototype = Object.append(new LSD.Object, LSD.Object.Group.prototype)
+LSD.Object.Group.prototype = Object.append(new LSD.Object, LSD.Object.Group.prototype);
+
+
+LSD.Object.Group.Array = function() {
+  LSD.Object.apply(this, arguments);
+};
+LSD.Object.Group.Array.prototype = new LSD.Object.Group;
+LSD.Object.Group.Array.prototype.___constructor = LSD.Array
+
+
+
+/*
+  Group struct has LSD.Object.Group its base object. 
+  To put it simply, it's an hash of arrays. 
+*/
+
+LSD.Struct.Group = function(properties) {
+  if (!properties) properties = {};
+  properties._constructor = LSD.Object.Group;
+  return LSD.Struct(properties)
+}
+
+LSD.Struct.Group.Array = function(properties) {
+  if (!properties) properties = {};
+  properties._constructor = LSD.Object.Group.Array;
+  return LSD.Struct(properties)
+}
