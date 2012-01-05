5b740a6f66577589110e1c44208a5504a3f80450
Yaroslaff Fedin <invizko@gmail.com>
2012-01-05 09:05:10 -0300

    Refactor Struct to lookup .properties method additionally to ._properties.

--- Source/Object/Struct.js
+++ Source/Object/Struct.js
@@ -46,59 +46,59 @@ LSD.Struct = function(properties) {
 };
 
 LSD.Struct.prototype = {
-  _onChange: function(name, value, state, old, memo) {
-    if (!this._properties) return;
-    var prop = this._properties[name];
+  _onChange: function(key, value, state, old, memo) {
+    var prop = (this._properties && this._properties[key]) || (this.properties && this.properties[key]);
     if (prop) {
+      var group = this._observed && this._observed[key]
+      if (group) {
+        if (state) group[2] = value;
+        else delete group[2];
+      }
       switch (typeof prop) {
         case 'function':
           var constructor = prop.prototype && prop.prototype._constructor;
           if (constructor) {
-            if (state && typeof this[name] == 'undefined') this._construct(name, prop, memo)
+            if (state && typeof this[key] == 'undefined') this._construct(key, prop, memo)
           } else {
             if (state) return prop.call(this, value, old);
             else return prop.call(this, undefined, value);
           }
           break;
         case 'string':
-          var obj = value
-          if (!state && obj != null && obj.mix) {
-            var stored = this._stored && this._stored[name];
-            if (stored != null) {
-              for (var i = 0, j = stored.length; i < j; i++)
-                if (obj != null) obj.mix(stored[i], null, false, true, false, this);
+          var stored = this._stored && this._stored[key];
+          if (stored != null && value != null) {
+            for (var i = 0, item; item = stored[i++];) {
+              if (value != null && (!item[2] || !item[2]._delegate || !item[2]._delegate(value, key, item[0], true, this)))
+                value.mix(item[0], item[1], memo, true, item[3], item[4]);
+              if (old != null && (!item[2] || !item[2]._delegate || !item[2]._delegate(old, key, item[0], false, this)))
+                old.mix(item[0], item[1], memo, false, item[3], item[4]);
             }
           }
       };
-      var group = this._observed && this._observed[name]
-      if (group) {
-        if (state) group[2] = value;
-        else delete group[2];
-      }
     }
     return value;
   },
   implement: function(object) {
     for (var name in object) this.prototype[name] = object[name]
   },
-  _construct: function(name, property, memo) {
-    var property = this._properties && this._properties[name];
+  _construct: function(key, property, memo) {
+    var property = (this._properties && this._properties[key]) || (this.properties && this.properties[key]);
     if (typeof property == 'string') {
       if (!this._observed) this._observed = {};
-      if (!this._observed[name]) {
-        this._observed[name] = [this, name];
-        this.watch(property, this._observed[name], false)
+      if (!this._observed[key]) {
+        this._observed[key] = [this, key];
+        this.watch(property, this._observed[key], false)
       }
-      return (this[name] || (this[name] = this.get(property, true)))
+      return (this[key] || (this[key] = this.get(property, true)))
     }
     if (this._delegate && !memo) memo = this;
-    return LSD.Object.prototype._construct.call(this, name, property, memo);
+    return LSD.Object.prototype._construct.call(this, key, property, memo);
   },
   _getConstructor: function(name) {
-    var object = this._properties;
-    if (object && (object = object[name])) {
-      var proto = object.prototype;
-      if (proto && proto._constructor) return object;
+    var property = (this._properties && this._properties[name]) || (this.properties && this.properties[name]);
+    if (property) {
+      var proto = property.prototype;
+      if (proto && proto._constructor) return property;
     }
     return this._constructor;
   }
